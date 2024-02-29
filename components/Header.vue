a30d39dba599a4f3d9d559e406032554104c9a0f
ristinuraulia2 <144652936+ristinuraulia2@users.noreply.github.com>
2024-02-29 09:42:07 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-lg-1">
+      <img src="~/assets/img/logo.png" alt="logo SMK4" class="logo">
+    </div>
+      <div class="col-lg-11">
+        <h2>Perpus Digital</h2>
+        <h3>SMKN 4 TASIKMALAYA</h3>
+        <address>
+          Jl.Depok, sukamenak, purbaratu
+        </address>
+      </div>
+  </div>
+</template>
+
+<style scoped>
+.logo{
+  width: 100px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container-fluid">
+    <div class="row my-5">
+      <div class="col-lg-6">
+        <nuxt-link to="/pengunjung/tambah">
+          <div class="card bg-pengunjung rounded-5">
+            <div class="card-body">
+              <h2>Pengunjung</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+
+      <div class="col-lg-6">
+        <nuxt-link to="/buku">
+          <div class="card bg-buku rounded-5">
+            <div class="card-body">
+              <h2>Cari Buku</h2>
+            </div>
+          </div>
+        </nuxt-link>
+        
+      </div>
+    </div>
+    <h2>STATISTIK</h2>
+    <div class="container-fluid">
+        <div class="row justify-content-evenly rounded-5">
+          <div class="col-5">
+            <div class="raccing">
+              <h2><strong>3</strong> Pengunjung</h2>
+              </div>
+            </div>
+            <div class="col-5">
+              <div class="raccing1">
+                <h2><strong>180</strong>Buku</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+  height: 250px;
+  box-shadow: 1px 1px 1px #424242;
+}
+.card.bg-pengunjung {
+  background-image: url("../assets/img/bg-home-kunjungan.jpg");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+}
+.card.bg-buku {
+  background: url("../assets/img/bg-home-cari-buku.jpg") no-repeat center center;
+  background-size: cover;
+}
+</style>
