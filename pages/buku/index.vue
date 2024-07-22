9b402165507bab4b450e8e18dcaf57db3d04c39b
ristinuraulia2 <144652936+ristinuraulia2@users.noreply.github.com>
2024-07-22 14:52:58 +0700

    Update index.vue

--- pages/buku/index.vue
+++ pages/buku/index.vue
@@ -1,114 +1,60 @@
 <template>
+  <div class="content">
     <div class="container-fluid">
       <div class="row">
         <div class="col-lg-12">
-          <h2 class="text-center my-4">BUKU</h2>
-              <div class="my-3">
-                <input type ="search" class="form-control rounded-5" placeholder ="Mau baca apa hari ini?">
+          <form @submit.prevent="getBooks" class="col mb-3">
+            <h2 class="text-center my-4 fw-bold">BUKU</h2>
+              <div class="input-group flex-nowrap rounded">
+                <input v-model="keyword" type="search" class="form-control from-control-lg rounded-5" placeholder="Mau Baca Buku Apa Hari Ini?..." aria-label="Search" @input="getbooks" />
               </div>
-                <div class="ny-3 text-muted">manampilkan 3 dari 3</div>
-                <div class="row">
-                  <div class="col-lg-2">
-                    <div class="card mb-3">
-                      <div class="card-body">
-                        <nuxt-link to="/buku/buku3">
-                        <img src="~/assets/img/buyya.jpeg" class="cover"  alt="cover 1">
-                      </nuxt-link>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-lg-2">
-                    <div class="card mb-3">
-                      <div class="card-body">
-                        <nuxt-link to="/buku/buku1">
-                          <img src="~/assets/img/tere.jpeg" class="cover"  alt="cover 2">
-                        </nuxt-link>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-lg-2">
-                    <div class="card mb-3">
-                      <div class="card-body">
-                        <nuxt-link to="/buku/buku2">
-                        <img src="~/assets/img/uhibbu.jpg" class="cover"  alt="cover 3">
-                        </nuxt-link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            </form>
+            <div class="my-3">Rekomendasi Buku {{ books.length }}  {{ jmlbuku }}</div>
           </div>
-      </template>
-  
+        </div>
+          <div class="row shadow-lg">
+              <div v-for="(book,i) in books" :key="i" class="col">
+                  <div class="card mb-3 col-lg-2 col-2">
+                    <nuxt-link :to="`/buku/${book.id}`">
+                      <div class="card-body">
+                        <img :src="book.cover" class="cover" alt="cover">
+                      </div>
+                    </nuxt-link>
+                  </div>
+              </div>
+          </div>
+    </div>
+    <nuxt-link to="/pengunjung">
+            <button type="button" class="btn btn-outline-dark mt-4 btn-lg">KEMBALI</button>
+          </nuxt-link>
+  </div>
+</template>
+
 <script setup>
+const supabase= useSupabaseClient()
 
-</script>
-
-<script setup>
-const supabase = useSupabaseClient()
-
-</script>
-
-<script setup>
-const Supabase = useSupabaseClient()
-
+const keyword = ref('')
 const books = ref([])
-</script>
+const jmlbuku = ref(0)
 
-<script setup>
-
-const getBooks = async () => {
-  const { data,eror } = await supabase.from('buku').select(`*, kategori(*)`)
+const getbooks = async () => {
+  const { data ,error } = await supabase
+  .from('buku')
+  .select(`*, kategori(*)`)
+  .ilike("judul", `%${keyword.value}%`)
+  .order('id')
   if(data) books.value = data
 }
-</script>
-
-<script setup>
-
-  onMounted(() => {
-    getBooks()
-  })
-</script>
-
-<div v-for="(book,i) in books" :key=""i class="col-lg-2">
-  <div class="card mb-3">
-    <div class="card-body">
-      <img :src="book.cover" class="cover" alt="cover">
-    </div>
-  </div>
-</div>
-</script>
-
-<form @submit.prevent="getBooks">
-  <input v-model="keyword" type="search">
-</form>
-
-<script setup>
-
-  const keyword = ref(**)
-
-</script>
-
-<script setup>
-
-  const getBooks= async () => {
-    const { data,error } await supabase.from 'buku'.select('*, kategori(*)')
-    .ilike('judul', '%${keyword.value}%')
-    id (data) books.value=data
-  }
-</script>
-
-  <style scoped>
-  .card-body {
-    width: 100%;
-    height: 20em;
-    padding: 0;
+const getJmlBuku = async () => {
+  const{ data, count } = await supabase
+    .from("buku") 
+    .select('*', { count: "exact" })
+    if(data) jmlbuku.value = count
 }
-  .cover {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: 30;
-}
-</style>
+
+onMounted(() =>{
+  getbooks()
+  getJmlBuku()
+
+})
+</script>
